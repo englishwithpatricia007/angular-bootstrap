edfed9b4f4e2cd069ec2a878ed90aef893b58413
Patricia <englishwithpatricia007@gmail.com>
2025-05-28 20:35:17 -0300

    lista de certificados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./_components/navbar/navbar.component";
 import { CommonModule } from '@angular/common';
 
+import { ItemCertificadoComponent } from "./_components/item-certificado/item-certificado.component";
+import { BaseUiComponent } from "./_components/base-ui/base-ui.component";
+import { CertificadosComponent } from "./pages/certificados/certificados.component";
+
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, CommonModule],
+  imports: [NavbarComponent, CommonModule, BaseUiComponent, CertificadosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
