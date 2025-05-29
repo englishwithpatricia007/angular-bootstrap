ca39040eb7a11f719ddd62e0e89e968459484caa
Patricia <englishwithpatricia007@gmail.com>
2025-05-29 20:56:48 -0300

    usando o router para navegação via ts

--- src/app/_components/item-certificado/item-certificado.component.ts
+++ src/app/_components/item-certificado/item-certificado.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { SecondaryButtonComponent } from "../secondary-button/secondary-button.component";
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-item-certificado',
-  imports: [SecondaryButtonComponent, RouterLink],
+  imports: [SecondaryButtonComponent],
   templateUrl: './item-certificado.component.html',
   styleUrl: './item-certificado.component.css'
 })
 export class ItemCertificadoComponent {
   id: string = '6';
+
+  constructor(private router: Router) { }
+
+  redirecionaCertificado() {
+    this.router.navigate(['/certificados', this.id]);
+  }
 
 }
